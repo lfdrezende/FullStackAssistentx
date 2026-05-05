2534c71211fa110d00208972d66ce140b225ec27
Lucas Rezende <lfdrezende@icloud.com>
2026-05-05 15:07:34 -0300

    primeiro commit

---
+++ backend/src/main/java/br/com/fiap/assistentx/AssistentxApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.assistentx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistentxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistentxApplication.class, args);
+	}
+
+}
