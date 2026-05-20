b12ab680cb3fdf720f7613883005db5cffe8691c
Lucas Rezende <lfdrezende@icloud.com>
2026-05-20 18:00:32 -0300

    Adição de verificação de usuario e senha com token fake

---
+++ backend/src/main/java/br/com/fiap/assistentx/dto/UsuarioSenhaDTO.java
@@ -0,0 +1,23 @@
+package br.com.fiap.assistentx.dto;
+
+public class UsuarioModeloDTO {
+
+    private Integer usuarioId;
+    private Integer modeloId;
+
+    public Integer getUsuarioId() {
+        return usuarioId;
+    }
+
+    public Integer getModeloId() {
+        return modeloId;
+    }
+
+    public void setUsuarioId(Integer usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public void setModeloId(Integer modeloId) {
+        this.modeloId = modeloId;
+    }
+}
---
+++ backend/src/main/java/br/com/fiap/assistentx/filter/TokenFilter.java
@@ -0,0 +1,4 @@
+package br.com.fiap.assistentx.filter;
+
+public class loginFilter {
+}
